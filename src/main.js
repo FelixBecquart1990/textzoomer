c0056f851c7b68048e483c00a0b863b4ac769e4f
Felix Becquart <felixbecquart1990@gmail.com>
2018-05-20 02:36:49 +0200

    UX experiment

--- src/main.js
+++ src/main.js
@@ -1,25 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from "vue";
+import App from "./App";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.use(Vuetify, { theme: {
-  primary: '#ee44aa',
-  secondary: '#424242',
-  accent: '#82B1FF',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-}})
+var VueTouch = require("vue-touch");
+Vue.use(VueTouch, { name: "v-touch" });
 
-Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#082129",
+    secondary: "#424242",
+    accent: "#82B1FF",
+    error: "#FF5252",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FFC107"
+  }
+});
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>",
+  created() {
+    // banner
+    let self = this;
+    window.addEventListener("beforeinstallprompt", function(event) {
+      event.preventDefault();
+      return false;
+    });
+  }
+});
