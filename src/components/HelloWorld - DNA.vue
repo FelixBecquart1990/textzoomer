0829e76e38de19f21303d25e2eb2ffaeacb00d1a
Felix Becquart <felixbecquart1990@gmail.com>
2018-05-28 16:26:27 +0200

    text from bot

---
+++ src/components/HelloWorld - DNA.vue
@@ -0,0 +1,178 @@
+<template>
+<div class="dna" style="height:calc(100% + 50px)"  @click="fullScreen()">
+  <div style="position:fixed;top:30px;color:white;font-family:arial; margin-left:10px;font-size:40px;text-shadow: 0px 4px #000000, 0px 2px #000000;">DNA & Genes</div>
+    <div class="ma-2 pa-3" style=";position:fixed;top:100px;">
+        <div class="gesture" style="z-index:999" @click="dialog = true"></div>
+        <div class="gesturer"></div>
+    <v-touch v-on:pinchout="remove" v-on:pinchin="add" style="overflow: scroll;;padding:12px;border-radius:6px;position:fixed;top:100px;right:10px;left:10px;bottom:10px; background-color:white">
+<transition name="fade">
+<span key="span-1" v-if="size>2">A section of DNA that contains instructions to make a protein is called a gene.</span>
+</transition>
+<transition name="fade">
+<span key="span-2" v-if="size>=10"> Each gene has the sequence for at least one polypeptide.</span>
+</transition>
+<transition name="fade">
+<span key="span-3" v-if="size>5"> Proteins form structures, and also form enzymes.</span>
+</transition>
+<transition name="fade">
+<span key="span-4"> The enzymes do most of the work in cells.</span>
+</transition>
+<transition name="fade">
+<span key="span-5" v-if="size>8"> Proteins are made out of smaller polypeptides, which are formed of amino acids.</span>
+</transition>
+<transition name="fade">
+<span key="span-6" v-if="size>6"> To make a protein to do a particular job, the correct amino acids have to be joined up in the correct order.</span>
+</transition>
+<transition name="fade">
+<span key="span-7" v-if="size>7"> Proteins are made by tiny machines in the cell called ribosomes.</span>
+</transition>
+<transition name="fade">
+<span key="span-8" v-if="size>1"> Ribosomes are in the main body of the cell, but DNA is only in the nucleus of the cell.</span>
+</transition>
+<transition name="fade">
+<span key="span-9" v-if="size>=10"> The codon is part of the DNA, but DNA never leaves the nucleus.</span>
+</transition>
+<transition name="fade">
+<span key="span-10" v-if="size>9"> Because DNA cannot leave the nucleus, the cell makes a copy of the DNA sequence in RNA.</span>
+</transition>
+<transition name="fade">
+<span key="span-11" v-if="size>8"> This is smaller and can get through the holes – pores – in the membrane of the nucleus and out into the cell.</span>
+</transition>
+<transition name="fade">
+<span key="span-12" v-if="size>7"> Genes encoded in DNA are transcribed into messenger RNA (mRNA) by proteins such as RNA polymerase.</span>
+</transition>
+<transition name="fade">
+<span key="span-13" v-if="size>3"> Mature mRNA is then used as a template for protein synthesis by the ribosome.</span>
+</transition>
+<transition name="fade">
+<span key="span-14" v-if="size>3"> Ribosomes read codons, 'words' made of three base pairs that tell the ribosome which amino acid to add.</span>
+</transition>
+<transition name="fade">
+<span key="span-15" v-if="size>4"> The ribosome scans along an mRNA, reading the code while it makes protein.</span>
+</transition>
+<transition name="fade">
+<span key="span-16" v-if="size>9"> Another RNA called tRNA helps match the right amino acid to each codon.</span>
+</transition>
+</v-touch>
+<v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title style="font-family:arial" class="headline">User experience experimentation</v-card-title>
+        <v-card-text style="font-family:arial"><span style="color:red">&#9755; Don't pay attention to the quality of summerization.</span><span style="color:green"> The only prupose of this Web page is to experiment pinch gesture to develop or summerize any texts.</span><br>This UX innovation might be useful for articles, Wikipedia etc. <v-expansion-panel class="elevation-0"><v-expansion-panel-content>
+      <div slot="header" style="font-family:arial;font-size:13px;margin-left:-22px;color:rgba(0,0,0,0.8)">More information</div>
+      <v-card>
+        <v-card-text style="font-family:arial">Click <a target="_blank" href="http://fastforwardlabs.github.io/luhn/">here</a> to understand how a text can be automaticaly summerized (that website is not responsive. Only available from desktop).<br>Obviously, summerization quality is still very poor today. But with AI, we can be sure that it won't take long to get hyper quality automatic summerization.</v-card-text>
+      </v-card>
+    </v-expansion-panel-content></v-expansion-panel></v-card-text>
+    <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn style="font-family:arial" color="primary" flat="flat" @click.native="dialog = false">ok</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      size: 5,
+      dialog: false
+    };
+  },
+  mounted() {
+    this.dialog = true;
+  },
+  methods: {
+    fullScreen() {
+      // auto fullscreen
+      var doc = window.document;
+      var docEl = doc.documentElement;
+      var requestFullScreen =
+        docEl.requestFullscreen ||
+        docEl.mozRequestFullScreen ||
+        docEl.webkitRequestFullScreen ||
+        docEl.msRequestFullscreen;
+      if (
+        !doc.fullscreenElement &&
+        !doc.mozFullScreenElement &&
+        !doc.webkitFullscreenElement &&
+        !doc.msFullscreenElement
+      ) {
+        requestFullScreen.call(window.document.documentElement);
+      }
+    },
+    add: function() {
+      this.size -= 0.2;
+      if (this.size <= 0) {
+        this.size = 0;
+      }
+    },
+    remove: function() {
+      this.size += 0.2;
+      if (this.size >= 10) {
+        this.size = 10;
+      }
+    }
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.gesture {
+  height: 45px;
+  width: 45px;
+  border-radius: 50%;
+  background: url("../../static/pinch1.gif") no-repeat 0 0;
+  background-size: 45px;
+  position: fixed;
+  top: 10px;
+  right: 10px;
+}
+.gesturer {
+  height: 55px;
+  width: 55px;
+  border-radius: 50%;
+  position: fixed;
+  top: 5px;
+  right: 5px;
+  border: solid 2px rgba(255, 255, 255, 0.7);
+  animation: Test 4s infinite;
+}
+@keyframes Test {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+.dna {
+  background: url("../../static/source.gif") 0 0;
+  background-position: center top;
+  background-size: 100% auto;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/components/HelloWorld - Lorem.vue
+++ src/components/HelloWorld - Lorem.vue
@@ -1,45 +1,28 @@
 <template>
-<div>
+<div @click="fullScreen">
     <div  class="gesturer"></div>
-
-<!--v-btn @click="add">add</v-btn>
-  <v-btn @click="remove">remove</v-btn-->
 
   <v-tooltip left>
       <div slot="activator" class="gesture" style="z-index:999"></div>
 
-        <span style="color:white">Pinch on the text of the article</span>
+        <span style="color:white">Pinch the article text</span>
     </v-tooltip>
   <div style="padding:15px; border-bottom:solid 1px rgba(0,0,0,0.1)" >
     <svg class="svgIcon-use" width="138" height="27" viewBox="0 0 138 27"><path d="M130 27V16.96c0-3.26-.154-5.472-2.437-5.472-1.16 0-2.138.57-2.863 1.512.217.906.3 1.968.3 3.127 0 2.247.036 5.11 0 7.973 0 .472-.046.58.244.87L127 27h-8V16.96c0-3.297-.461-5.472-2.708-5.472-1.16 0-1.64.653-2.292 1.595V24.1c0 .472-.026.58.3.87L116 27h-8V11.56c0-.47-.07-.579-.36-.905L106 9h8v3.612c.906-2.537 2.437-4.112 5.372-4.112 2.682 0 4.494 1.466 5.255 4.257.834-2.392 3.008-4.257 6.053-4.257 3.588 0 5.32 2.626 5.32 7.627 0 2.392.036 5.11 0 7.973 0 .472.004.652.25.87L138 27h-8zm-27-3.045c0 .472-.149.617.178.906L105 27h-8v-4c-.906 2.465-2.956 4-5.637 4C87.775 27 86 24.39 86 19.461c0-2.391-.036-5 0-7.936 0-.471-.11-.58-.4-.87L84 9h8v9.628c0 3.225.269 5.4 2.298 5.4 1.16 0 2.086-.725 2.702-1.63V11.56c0-.471-.129-.58-.419-.906L95 9h8v14.955zM78.002.25A3.248 3.248 0 0 1 81.25 3.5 3.25 3.25 0 1 1 78.002.25zM75 27V11.56c0-.47.168-.579-.122-.905L73 9h8v15.1c0 .472-.01.678.24.9L83 27h-8zM64 11.706c-.507-.652-1.418-1.123-2.396-1.123-1.957 0-3.842 1.775-3.842 7.03 0 4.93 1.631 6.669 3.66 6.669.907 0 1.853-.436 2.578-1.378V11.706zm6 12.286c0 .47-.026.58.3.87L72 27h-8v-3.697C62.913 25.804 60.951 27 58.632 27 54.5 27 51.5 23.738 51.5 17.795c0-5.582 3.254-9.314 7.784-9.314 2.356 0 3.919 1.123 4.716 2.899V3.878c0-.471-.077-.617-.403-.906L62 1.305 70 .29v23.702zM43.9 16c.037-.471.037-.67.037-.815 0-4.747-.937-5.435-2.437-5.435-1.5 0-2.854.995-2.927 6.25h5.328zm-5.327 1c0 4.711 2.392 6.63 5.183 6.63 2.174 0 4.313-.943 5.509-3.335h.072c-.942 4.566-3.77 6.705-8.01 6.705-4.566 0-8.879-2.755-8.879-9.133 0-6.705 4.277-9.386 9.097-9.386 3.842 0 7.937 1.811 7.937 7.646 0 .109 0 .438-.036.873H38.573zM31.5 27h-12l2.39-2.646c.084-.084.11-.399.11-.87V7l-7.866 20L5.581 8.372C5.364 7.9 5.181 7.285 5 6.777V20.62c0 .58-.035.653.364 1.196L9 27H0l3.64-5.183c.399-.543.36-.616.36-1.196V6.27c0-.617.095-.69-.195-1.051L1 1h8.495l7.355 16.3L23.24 1h8.26l-2.2 2.75c-.326.326-.3.599-.3 1.106v18.629c0 .47.005.75.138.87L31.5 27z"></path></svg>
   </div>
-<div style="padding-right:18px;padding-left:18px">
+  <!--img src="../../static/img.jpg" style="width:100%"/>
+  <v-btn @click="add">add</v-btn>
+  <v-btn @click="remove">remove</v-btn-->
+  <div style="padding:18px">
     <h1 style="font-weight:bold;font-size:30px;margin-top:-20px;margin-bottom:10px;margin-top:0px">Zoom in or zoom out on the text bellow</h1>
-    <v-touch v-on:pinchout="remove" v-on:pinchin="add" v-on:swipeup="swipeup" v-on:swipedown="swipedown" style="min-height:400px;">
-<transition name="fade">
-<span key="span-1" v-if="size>3">What if I tell you that some of the most recent news articles you’ve read were written by an automated AI-machine?</span>
-</transition>
-<transition name="fade">
-<span key="span-2"> News giants have steadily been adapting algorithms, programming them in several software projects to introduce automated news writing in their newsrooms.</span>
-</transition>
-<transition name="fade">
-<span key="span-3" v-if="size>1"> According to Matt Carlson, author of “The Robotic Reporter”, it is the algorithmic process that converts data into narrative news texts with limited to no human intervention beyond the initial programming.</span>
-</transition>
-<transition name="fade">
-<span key="span-4" v-if="size>5"> The way news is produced and distributed was subject to an inevitable change.</span>
-</transition>
-<transition name="fade">
-<span key="span-5" v-if="size>2"> Respected publishers like Associated Press quickly understood this, and since 2014 they have been using the services of Automated Insights, the creators of “Wordsmith”, a robot platform able to convert data from reports into news stories.</span>
-</transition>
-<transition name="fade">
-<span key="span-6" v-if="size>6"> The folks from AP aren’t the only ones, LA Times employs a robot that collects information on every homicide committed in the city of Los Angeles, the site created by the machine called “Homicide Report” utilizes a robot-reporter with the ability to include in its reports tons of data that includes: the victim’s gender and race, cause of death, officer involvement, neighborhood, and year of death.</span>
-</transition>
-<transition name="fade">
-<span key="span-7" v-if="size>4"> Other outlets like The Guardian, The Washington Post, and Forbes.com have also experimented with automated news generators provided by Narrative Science, a start-up headquartered in Chicago, whose co-founder Kristian Hammond, once said that within 15 years more than 90% percent of news would be written by a computer.</span>
-</transition>
-</v-touch>
-
-</div>
+    <v-touch style="min-height:400px;" v-on:pinchout="remove" v-on:pinchin="add" v-on:swipeup="swipeup" v-on:swipedown="swipedown">
+      <div v-for="line in arrayComputed" :key="line+Math.random()" style="display:inline-block;">
+          <span  v-if="size>=line.size" style="margin-right:4px; font-size:17px">
+            {{ line.word }}
+          </span>
+      </div>
+    </v-touch>
+  </div>
 </div>
 </template>
 
@@ -47,37 +30,65 @@
 export default {
   data() {
     return {
-      size: 3,
-      dialog: false
+      size: 5,
+      text:
+        "Etenim si attendere diligenter, existimare vere de omni hac causa volueritis, sic constituetis, iudices, nec descensurum quemquam ad hanc accusationem fuisse, cui, utrum vellet, liceret, nec, cum descendisset, quicquam habiturum spei fuisse, nisi alicuius intolerabili libidine et nimis acerbo odio niteretur. Sed ego Atratino, humanissimo atque optimo adulescenti meo necessario, ignosco, qui habet excusationem vel pietatis vel necessitatis vel aetatis. Si voluit accusare, pietati tribuo, si iussus est, necessitati, si speravit aliquid, pueritiae. Ceteris non modo nihil ignoscendum, sed etiam acriter est resistendum. Quam ob rem id primum videamus, si placet, quatenus amor in amicitia progredi debeat. Numne, si Coriolanus habuit amicos, ferre contra patriam arma illi cum Coriolano debuerunt? num Vecellinum amici regnum adpetentem, num Maelium debuerunt iuvare? Nam quibusdam, quos audio sapientes habitos in Graecia, placuisse opinor mirabilia quaedam (sed nihil est quod illi non persequantur argutiis): partim fugiendas esse nimias amicitias, ne necesse sit unum sollicitum esse pro pluribus; satis superque esse sibi suarum cuique rerum, alienis nimis implicari molestum esse; commodissimum.",
+      array: [],
+      arrayComputed: []
     };
   },
-  mounted() {},
+  mounted() {
+    this.array = this.text.split(" ");
+    for (let word in this.array) {
+      this.arrayComputed.push({
+        word: this.array[word] + " ",
+        size: Math.floor(Math.random() * 10) + 0.5
+      });
+    }
+  },
   methods: {
-    swipeup() {
-      window.document.documentElement.scrollTop += 1000;
+    swipeup(){
+      window.document.documentElement.scrollTop+=1000
     },
-    swipedown() {
-      window.document.documentElement.scrollTop -= 1000;
+    swipedown(){
+      window.document.documentElement.scrollTop-=1000
+    },
+    fullScreen() {
+      // auto fullscreen
+      var doc = window.document;
+      var docEl = doc.documentElement;
+      var requestFullScreen =
+        docEl.requestFullscreen ||
+        docEl.mozRequestFullScreen ||
+        docEl.webkitRequestFullScreen ||
+        docEl.msRequestFullscreen;
+      if (
+        !doc.fullscreenElement &&
+        !doc.mozFullScreenElement &&
+        !doc.webkitFullscreenElement &&
+        !doc.msFullscreenElement
+      ) {
+        requestFullScreen.call(window.document.documentElement);
+      }
     },
     add: function() {
-      this.size -= 0.2;
-      if (this.size <= 0) {
-        this.size = 0;
+      this.size -= 0.5;
+      if (this.size <= 0.5) {
+        this.size = 0.5;
       }
-      console.log(this.size);
     },
     remove: function() {
-      this.size += 0.2;
-      if (this.size >= 6.5) {
-        this.size = 6.5;
+      this.size += 0.5;
+      if (this.size >= 10) {
+        this.size = 10;
       }
-      console.log(this.size);
     }
   }
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 .gesture {
   height: 45px;
   width: 45px;
@@ -107,9 +118,8 @@ export default {
   }
 }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
@@ -130,3 +140,4 @@ a {
   color: #42b983;
 }
 </style>
+
